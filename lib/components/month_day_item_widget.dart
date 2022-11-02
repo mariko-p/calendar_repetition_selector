504fc61734f975c325d5b99df24a11c97d57b58d
Ivan Stepanic <stepanic.ivan@gmail.com>
2022-11-02 10:34:13 +0100

    Local changes before sync

--- lib/components/month_day_item_widget.dart
+++ lib/components/month_day_item_widget.dart
@@ -5,69 +5,108 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MonthDayItemWidget extends StatefulWidget {
-  const MonthDayItemWidget({
+  MonthDayItemWidget({
     Key? key,
     this.monthDay,
   }) : super(key: key);
 
-  final MonthDayStruct? monthDay;
+  MonthDayStruct? monthDay;
 
   @override
   _MonthDayItemWidgetState createState() => _MonthDayItemWidgetState();
 }
 
 class _MonthDayItemWidgetState extends State<MonthDayItemWidget> {
+  uncheckItem(int? index ) {
+    if (skipItem(index)) return;
+    setState(() {
+      var text = widget.monthDay?.text;
+      var index = widget.monthDay?.index;
+      var updatedMonthDay =
+          createMonthDayStruct(text: text, index: index, isChecked: false);
+
+      widget.monthDay = updatedMonthDay;
+    });
+  }
+
+  checkItem(int? index) {
+    if (skipItem(index)) return;
+    setState(() {
+      var text = widget.monthDay?.text;
+      var index = widget.monthDay?.index;
+      var updatedMonthDay =
+          createMonthDayStruct(text: text, index: index, isChecked: true);
+
+      widget.monthDay = updatedMonthDay;
+    });
+  }
+
+  bool skipItem(int? index) {
+    if (index == 31 || index == 32 || index == 33 || index == 34) {
+      return true;
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         if (widget.monthDay?.isChecked == true)
-          Container(
-            width: 32,
-            height: 32,
-            decoration: BoxDecoration(
-              color: Color(0xFF9980DD),
-              shape: BoxShape.circle,
-            ),
-            child: Align(
-              alignment: AlignmentDirectional(0, 0),
-              child: SelectionArea(
-                  child: Text(
-                valueOrDefault<String>(
-                  widget.monthDay?.text,
-                  '',
+          InkWell(
+            onTap: (() {
+              uncheckItem(widget.monthDay?.index);
+            }),
+            child: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                color: Color(0xFF9980DD),
+                shape: BoxShape.circle,
+              ),
+              child: Align(
+                alignment: AlignmentDirectional(0, 0),
+                child: Text(
+                  valueOrDefault<String>(
+                    widget.monthDay?.text,
+                    '',
+                  ),
+                  style: FlutterFlowTheme.of(context).bodyText1.override(
+                        fontFamily: 'Rubik',
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                      ),
                 ),
-                style: FlutterFlowTheme.of(context).bodyText1.override(
-                      fontFamily: 'Rubik',
-                      color: Colors.white,
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
-                    ),
-              )),
+              ),
             ),
           ),
         if (widget.monthDay?.isChecked == false)
-          Container(
-            width: 32,
-            height: 32,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-            ),
-            child: Align(
-              alignment: AlignmentDirectional(0, 0),
-              child: SelectionArea(
-                  child: Text(
-                valueOrDefault<String>(
-                  widget.monthDay?.text,
-                  '',
+          InkWell(
+            onTap: (() {
+              checkItem(widget.monthDay?.index);
+            }),
+            child: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+              child: Align(
+                alignment: AlignmentDirectional(0, 0),
+                child: Text(
+                  valueOrDefault<String>(
+                    widget.monthDay?.text,
+                    '',
+                  ),
+                  style: FlutterFlowTheme.of(context).bodyText1.override(
+                        fontFamily: 'Rubik',
+                        color: FlutterFlowTheme.of(context).primaryText,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                      ),
                 ),
-                style: FlutterFlowTheme.of(context).bodyText1.override(
-                      fontFamily: 'Rubik',
-                      color: FlutterFlowTheme.of(context).primaryText,
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
-                    ),
-              )),
+              ),
             ),
           ),
       ],
